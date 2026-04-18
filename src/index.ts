973ce49b6eeb10d92b8a387d8e25b37fb736857e
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:53 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,41 +17,28 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { ClayCalculatorComponent, ClayCalculatorSEO, ClayCalculatorBibliography } from './tool/clayCalculator';
 export { CLAY_CALCULATOR_TOOL } from './tool/clayCalculator/index';
 
-export { EpoxyCalculatorComponent, EpoxyCalculatorSEO, EpoxyCalculatorBibliography } from './tool/epoxyCalculator';
 export { EPOXY_CALCULATOR_TOOL } from './tool/epoxyCalculator/index';
 
-export { BalusterCalculatorComponent, BalusterCalculatorSEO, BalusterCalculatorBibliography } from './tool/balusterCalculator';
 export { BALUSTER_CALCULATOR_TOOL } from './tool/balusterCalculator/index';
 
-export { MortarCalculatorComponent, MortarCalculatorSEO, MortarCalculatorBibliography } from './tool/mortarCalculator';
 export { MORTAR_CALCULATOR_TOOL } from './tool/mortarCalculator/index';
 
-export { PassepartoutCalculatorComponent, PassepartoutCalculatorSEO, PassepartoutCalculatorBibliography } from './tool/passepartoutCalculator';
 export { PASSEPARTOUT_CALCULATOR_TOOL } from './tool/passepartoutCalculator/index';
 
-export { ConcreteCalculatorComponent, ConcreteCalculatorSEO, ConcreteCalculatorBibliography } from './tool/concreteCalculator';
 export { CONCRETE_CALCULATOR_TOOL } from './tool/concreteCalculator/index';
 
-export { CutOptimizerComponent, CutOptimizerSEO, CutOptimizerBibliography } from './tool/cutOptimizer';
 export { CUT_OPTIMIZER_TOOL } from './tool/cutOptimizer/index';
 
-export { VoltageDropCalculatorComponent, VoltageDropCalculatorSEO, VoltageDropCalculatorBibliography } from './tool/voltageDropCalculator';
 export { VOLTAGE_DROP_CALCULATOR_TOOL } from './tool/voltageDropCalculator/index';
 
-export { FurnitureFitComponent, FurnitureFitSEO, FurnitureFitBibliography } from './tool/furnitureFit';
 export { FURNITURE_FIT_TOOL } from './tool/furnitureFit/index';
 
-export { ThermalExpansionCalculatorComponent, ThermalExpansionCalculatorSEO, ThermalExpansionCalculatorBibliography } from './tool/thermalExpansionCalculator';
 export { THERMAL_EXPANSION_CALCULATOR_TOOL } from './tool/thermalExpansionCalculator/index';
 
-export { DrillCalculatorComponent, DrillCalculatorSEO, DrillCalculatorBibliography } from './tool/drillCalculator';
 export { DRILL_CALCULATOR_TOOL } from './tool/drillCalculator/index';
 
-export { StairCalculatorComponent, StairCalculatorSEO, StairCalculatorBibliography } from './tool/stairCalculator';
 export { STAIR_CALCULATOR_TOOL } from './tool/stairCalculator/index';
 
-export { DrillSharpenerComponent, DrillSharpenerSEO, DrillSharpenerBibliography } from './tool/drillSharpener';
 export { DRILL_SHARPENER_TOOL } from './tool/drillSharpener/index';
